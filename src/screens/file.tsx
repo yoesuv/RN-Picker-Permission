7ab9831e20743c898d1f5332b9c573afc7bc8d9d
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-31 14:15:38 +0700

    :art: ui choose file

--- src/screens/file.tsx
+++ src/screens/file.tsx
@@ -5,9 +5,35 @@ import AppButton from '../components/button';
 import SizedBox from '../components/sized-box';
 
 export default function File() {
-    return <SafeAreaView>
-        <View>
-            <Text>File Picker</Text>
+
+    const chooseFile = () => {
+
+    }
+
+    return <SafeAreaView style={styles.container}>
+        <View style={styles.content}>
+            <Text style={styles.label}>File Picker</Text>
+            <SizedBox height={24} />
+            <View style={styles.button}>
+                <AppButton title="Choose File" onPress={chooseFile} />
+            </View>
         </View>
     </SafeAreaView>
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    content: {
+        paddingTop: 24,
+        paddingHorizontal: 24,
+    },
+    label: {
+        fontSize: 16,
+    },
+    button: {
+        height: 50,
+    },
+})

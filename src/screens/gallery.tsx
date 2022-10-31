88febf0ff226e93bf841f4086f957bd64a01192c
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-31 09:10:05 +0700

    :pencil: setup open gallery

--- src/screens/gallery.tsx
+++ src/screens/gallery.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, SafeAreaView, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import AppButton from '../components/button';
 
 export default function Gallery() {
+
+    const [image, setImage] = useState(null)
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: false,
+            quality: 0.8,
+          });
+        console.log(`Gallery Screen # ${JSON.stringify(result)}`);
+        if (!result.cancelled) {
+
+        }
+    }
+
     return <SafeAreaView style={styles.container}>
         <View style={styles.content}>
             <View style={styles.button}>
-                <AppButton title="Open Gallery" onPress={() => {}} />
+                <AppButton title="Open Gallery" onPress={pickImage} />
             </View>
         </View>
     </SafeAreaView>

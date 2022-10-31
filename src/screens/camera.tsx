35fea06a4942f651816d139fa79ce26ab3750eac
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-31 13:37:33 +0700

    :art: ui take camera

--- src/screens/camera.tsx
+++ src/screens/camera.tsx
@@ -1,15 +1,55 @@
 import React, { useState } from 'react';
 import { StyleSheet, SafeAreaView, Text, View, Image } from 'react-native';
+import AppButton from '../components/button';
+import SizedBox from '../components/sized-box';
 
 export default function Camera() {
-    return <SafeAreaView style={styles.container}>
 
+    const pickCamera = async () => {
+        
+    }
+
+    return <SafeAreaView style={styles.container}>
+        <View style={styles.content}>
+            <View style={styles.contentImage}>
+                <TakeImage />
+            </View>
+            <SizedBox height={24} />
+            <View style={styles.button}>
+                <AppButton title="Open Camera" onPress={pickCamera} />
+            </View>
+        </View>
     </SafeAreaView>
+}
+
+function TakeImage(): JSX.Element {
+    return <View style={styles.contentSelect}>
+        <Text style={styles.labelSelect}>Take image from Camera</Text>
+    </View>
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    content: {
+        paddingTop: 24,
+        paddingHorizontal: 24,
+    },
+    contentImage: {
+        borderWidth: 1,
+        height: 200,
+    },
+    contentSelect: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    labelSelect: {
+        fontSize: 16,
+    },
+    button: {
+        height: 50,
     },
 })

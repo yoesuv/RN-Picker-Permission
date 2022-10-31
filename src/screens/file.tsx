5bf8e0a80a59b1f0ee821f0649caac0a3601e360
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-10-31 15:05:53 +0700

    :sparkles: function choose file

--- src/screens/file.tsx
+++ src/screens/file.tsx
@@ -1,24 +1,39 @@
 import React, { useState } from 'react';
 import { StyleSheet, SafeAreaView, Text, View, Image } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import * as DocumentPicker from 'expo-document-picker';
 import AppButton from '../components/button';
 import SizedBox from '../components/sized-box';
 
 export default function File() {
 
-    const chooseFile = () => {
+    const [file, setFile] = useState<any | null>(null);
+    const [fileName, setFileName] = useState<any | null>(null);
 
+    const chooseFile = async () => {
+        let result = await DocumentPicker.getDocumentAsync({
+            type: ['application/pdf', 'image/png'],
+        });
+        if (result.type === 'success') {
+            setFileName(result.name)
+            setFile(result.uri)
+        }
     }
 
     return <SafeAreaView style={styles.container}>
         <View style={styles.content}>
-            <Text style={styles.label}>File Picker</Text>
+            <Text style={styles.label}>File Name : { fileName }</Text>
+            <SizedBox height={24} />
+            { file != null ? <Text style={styles.label} >{ file }</Text> : <ChooseFile /> }
             <SizedBox height={24} />
             <View style={styles.button}>
                 <AppButton title="Choose File" onPress={chooseFile} />
             </View>
         </View>
     </SafeAreaView>
+}
+
+function ChooseFile(): JSX.Element {
+    return <Text style={styles.label}>Choose a File</Text>
 }
 
 const styles = StyleSheet.create({

e050c171b09857c06f17226d7c281160115448d4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-10-15 14:21:51 +0700

    :pencil: request notification permission

--- src/screens/notification.tsx
+++ src/screens/notification.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { StyleSheet, SafeAreaView, Text, View } from 'react-native';
-import SizedBox from '../components/sized-box';
+import React, { useEffect } from 'react';
+import { StyleSheet, SafeAreaView, View, Platform } from 'react-native';
 import AppButton from '../components/button';
 import * as Notifications from 'expo-notifications';
 
@@ -16,14 +15,45 @@ export default function NotificationScreen() {
       });
 
     const localNotification = async () => {
-        Notifications.scheduleNotificationAsync({
-            content: {
-                title:'Title Notification',
-                body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-            },
-            trigger: null,
-        });
+        const status = await Notifications.getPermissionsAsync();
+        if (status.granted) {
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title:'Title Notification',
+                    body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                },
+                trigger: null,
+            });
+        } else {
+            alert('Push Notifications Permission is not Granted');
+        }
     }
+
+    async function registerForPushNotificationsAsync() {
+        if (Platform.OS == 'android') {
+            await Notifications.setNotificationChannelAsync('default', {
+                name: 'default',
+                importance: Notifications.AndroidImportance.MAX,
+                vibrationPattern: [0, 250, 250, 250],
+                lightColor: '#FF231F7C',
+            });
+        }
+
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Push Notifications Permission is not Granted');
+            return;
+        }
+    }
+
+    useEffect(() => {
+        registerForPushNotificationsAsync();
+    },[]);
 
     return <SafeAreaView style={styles.container}>
         <View style={styles.content}>
